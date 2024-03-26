074bbe013c454ae075e04aee24c045b14c0eee73
musriadisyahruddin <musriadi.syahruddin@gmail.com>
2024-03-26 22:42:58 +0800

    "test live pages"

--- js/index.js
+++ js/index.js
@@ -1,16 +1,34 @@
 let links = document.querySelectorAll('a[href="#"]');
 
 links.forEach(function (link) {
-    link.addEventListener("click", function (event) {
-        event.preventDefault();
-    });
+  link.addEventListener("click", function (event) {
+    event.preventDefault();
+  });
 });
 
 document.querySelector(".navbar-toggle").addEventListener("click", function () {
-    let navbarMenu = document.querySelector(".navbar-menu");
-    if (navbarMenu.classList.contains("responsive")) {
-        navbarMenu.classList.remove("responsive");
-    } else {
-        navbarMenu.classList.add("responsive");
-    }
+  let navbarMenu = document.querySelector(".navbar-menu");
+  if (navbarMenu.classList.contains("show")) {
+    navbarMenu.classList.remove("show");
+  } else {
+    navbarMenu.classList.add("show");
+  }
+});
+
+function autoSlide() {
+  let currentSlide = 0;
+  let slides = document.querySelectorAll(".carousel-item");
+  let totalSlides = slides.length;
+  let carouselInner = document.querySelector(".carousel-inner");
+
+  setInterval(function () {
+    currentSlide = (currentSlide + 1) % totalSlides;
+    let slideWidth = slides[0].offsetWidth;
+    let translateX = -currentSlide * slideWidth;
+    carouselInner.style.transform = "translateX(" + translateX + "px)";
+  }, 3000);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  autoSlide();
 });

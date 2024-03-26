ba49110cdc1cbae9df5869b8bde43050b744f397
musriadisyahruddin <musriadi.syahruddin@gmail.com>
2024-03-27 00:10:16 +0800

    selesai

--- js/index.js
+++ js/index.js
@@ -7,13 +7,17 @@ links.forEach(function (link) {
 });
 
 document.querySelector(".navbar-toggle").addEventListener("click", function () {
+  toggleNavbar();
+});
+
+function toggleNavbar() {
   let navbarMenu = document.querySelector(".navbar-menu");
   if (navbarMenu.classList.contains("show")) {
     navbarMenu.classList.remove("show");
   } else {
     navbarMenu.classList.add("show");
   }
-});
+}
 
 function autoSlide() {
   let currentSlide = 0;
@@ -31,4 +35,48 @@ function autoSlide() {
 
 document.addEventListener("DOMContentLoaded", function () {
   autoSlide();
+});
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollToTopBtn.classList.add("active");
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.classList.remove("active");
+    scrollToTopBtn.removeAttribute("style");
+  }
+}
+
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+document
+  .getElementById("scrollToTopBtn")
+  .addEventListener("click", function () {
+    scrollToTop();
+  });
+
+var menuLinks = document.querySelectorAll(".navbar-menu a");
+
+menuLinks.forEach(function (menuLink) {
+  menuLink.addEventListener("click", function (e) {
+    e.preventDefault();
+    toggleNavbar();
+    var targetId = this.getAttribute("href");
+    var targetElement = document.querySelector(targetId);
+
+    if (targetElement) {
+      targetElement.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    }
+  });
 });
